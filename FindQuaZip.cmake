682f1b7d84cb4c6c457871e621508522224bb618
David Callu <callu.david@gmail.com>
2015-10-16 14:11:22 +0200

    chore(cmake): improve CMakeList.txt and FindQuaZip

--- FindQuaZip.cmake
+++ FindQuaZip.cmake
@@ -1,50 +1,73 @@
-# QUAZIP5_FOUND               - QuaZip library was found
-# QUAZIP5_INCLUDE_DIR         - Path to QuaZip include dir
-# QUAZIP5_INCLUDE_DIRS        - Path to QuaZip and zlib include dir (combined from QUAZIP_INCLUDE_DIR + ZLIB_INCLUDE_DIR)
-# QUAZIP5_LIBRARIES           - List of QuaZip libraries
-# QUAZIP5_ZLIB_INCLUDE_DIR    - The include dir of zlib headers
+# QuaZip5_FOUND               - QuaZip library was found
+# QuaZip5_INCLUDE_DIRS        - Path to QuaZip and zlib include dir (combined from QUAZIP_INCLUDE_DIR + ZLIB_INCLUDE_DIR)
+# QuaZip5_LIBRARIES           - List of QuaZip libraries
+#
+# quazip5 imported library interface
 
+if (QUAZIP_USE_STATIC)
+    set(_QUAZIP_STATIC_SUFFIX _static)
+    set(_QUAZIP_LIBRARY_MODE STATIC)
+else()
+    set(_QUAZIP_LIBRARY_MODE SHARED)
+endif()
+
+set(_QUAZIP_QT_VERSION_SUFFIX "")
+if (QUAZIP_USE_QT5)
+    set(_QUAZIP_QT_VERSION_SUFFIX 5)
+endif()
+
+set(QUAZIP_NAME QUAZIP${_QUAZIP_QT_VERSION_SUFFIX})
+set(QuaZip_NAME QuaZip${_QUAZIP_QT_VERSION_SUFFIX})
+set(quazip_NAME quazip${_QUAZIP_QT_VERSION_SUFFIX})
+set(quazip_LIB_NAME quazip${_QUAZIP_QT_VERSION_SUFFIX}${_QUAZIP_STATIC_SUFFIX})
+
+set(_QUAZIP_SEARCH_PATHS $ENV{${QUAZIP_UP_NAME}_DIR}
+                         $ENV{${QUAZIP_UP_NAME}_ROOT} )
 
 find_path(
-    QuaZip_INCLUDE_DIR quazip5/quazip.h
-    PATHS $ENV{QUAZIP_DIR}
-          $ENV{QUAZIP_ROOT}
+    ${QuaZip_NAME}_INCLUDE_DIR ${quazip_NAME}/quazip.h
+    PATHS ${_QUAZIP_SEARCH_PATHS}
     PATH_SUFFIXES include
 )
 
+
 find_library(
-    QuaZip_LIBRARY_RELEASE quazip
-    PATHS $ENV{QUAZIP_DIR}
-          $ENV{QUAZIP_ROOT}
+    ${QuaZip_NAME}_LIBRARY_RELEASE ${quazip_LIB_NAME}
+    PATHS ${_QUAZIP_SEARCH_PATHS}
     PATH_SUFFIXES lib
 )
 
 find_library(
-    QuaZip_LIBRARY_DEBUG quazipd
-    PATHS $ENV{QUAZIP_DIR}
-          $ENV{QUAZIP_ROOT}
+    ${QuaZip_NAME}_LIBRARY_DEBUG ${quazip_LIB_NAME}d
+    PATHS ${_QUAZIP_SEARCH_PATHS}
     PATH_SUFFIXES lib
 )
 
-if(QuaZip_LIBRARY_RELEASE OR QuaZip_LIBRARY_DEBUG)
+if(${QuaZip_NAME}_LIBRARY_RELEASE OR ${QuaZip_NAME}_LIBRARY_DEBUG)
     set(
-        QuaZip_LIBRARIES
-        release ${QuaZip_LIBRARY_RELEASE}
-	debug ${QuaZip_LIBRARY_DEBUG}
+        ${QuaZip_NAME}_LIBRARIES
+        release ${${QuaZip_NAME}_LIBRARY_RELEASE}
+        debug ${${QuaZip_NAME}_LIBRARY_DEBUG}
     )
 endif()
 
-if(QuaZip_INCLUDE_DIR)
+if(${QuaZip_NAME}_INCLUDE_DIR)
     set(
-        QuaZip_INCLUDE_DIRS
-        ${QuaZip_INCLUDE_DIR}
+        ${QuaZip_NAME}_INCLUDE_DIRS
+        ${${QuaZip_NAME}_INCLUDE_DIR}
     )
 endif()
 
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-    QuaZip FOUND_VAR QuaZip_FOUND
-    REQUIRED_VARS QuaZip_INCLUDE_DIRS QuaZip_LIBRARIES
-    VERSION_VAR QuaZip_VERSION_STRING
+    ${QuaZip_NAME} FOUND_VAR ${QuaZip_NAME}_FOUND
+    REQUIRED_VARS ${QuaZip_NAME}_INCLUDE_DIRS ${QuaZip_NAME}_LIBRARIES
 )
+
+
+add_library(${quazip_LIB_NAME} ${_QUAZIP_LIBRARY_MODE} IMPORTED)
+
+set_property(${quazip_LIB_NAME} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${${QuaZip_NAME}_INCLUDE_DIRS})
+set_property(${quazip_LIB_NAME} PROPERTIES INTERFACE_LINK_LIBRARIES_RELEASE ${${QuaZip_NAME}_LIBRARY_RELEASE})
+set_property(${quazip_LIB_NAME} PROPERTIES INTERFACE_LINK_LIBRARIES_DEBUG ${${QuaZip_NAME}_LIBRARY_DEBUG})
