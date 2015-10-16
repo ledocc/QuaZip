6a98ccdb3786e9f08d6990ec814775bcf63091e5
David Callu <callu.david@gmail.com>
2015-10-16 08:06:13 +0200

    chore(cmake): fix build process

--- FindQuaZip.cmake
+++ FindQuaZip.cmake
@@ -1,43 +1,50 @@
-# QUAZIP_FOUND               - QuaZip library was found
-# QUAZIP_INCLUDE_DIR         - Path to QuaZip include dir
-# QUAZIP_INCLUDE_DIRS        - Path to QuaZip and zlib include dir (combined from QUAZIP_INCLUDE_DIR + ZLIB_INCLUDE_DIR)
-# QUAZIP_LIBRARIES           - List of QuaZip libraries
-# QUAZIP_ZLIB_INCLUDE_DIR    - The include dir of zlib headers
+# QUAZIP5_FOUND               - QuaZip library was found
+# QUAZIP5_INCLUDE_DIR         - Path to QuaZip include dir
+# QUAZIP5_INCLUDE_DIRS        - Path to QuaZip and zlib include dir (combined from QUAZIP_INCLUDE_DIR + ZLIB_INCLUDE_DIR)
+# QUAZIP5_LIBRARIES           - List of QuaZip libraries
+# QUAZIP5_ZLIB_INCLUDE_DIR    - The include dir of zlib headers
 
 
-IF (QUAZIP_INCLUDE_DIRS AND QUAZIP_LIBRARIES)
-	# in cache already
-	SET(QUAZIP_FOUND TRUE)
-ELSE (QUAZIP_INCLUDE_DIRS AND QUAZIP_LIBRARIES)
-    IF (Qt5Core_FOUND)
-        set(QUAZIP_LIB_VERSION_SUFFIX 5)
-    ENDIF()
-	IF (WIN32)
-		FIND_PATH(QUAZIP_LIBRARY_DIR
-			WIN32_DEBUG_POSTFIX d
-            NAMES libquazip${QUAZIP_LIB_VERSION_SUFFIX}.dll
-			HINTS "C:/Programme/" "C:/Program Files"
-			PATH_SUFFIXES QuaZip/lib
-		)
-        FIND_LIBRARY(QUAZIP_LIBRARIES NAMES libquazip${QUAZIP_LIB_VERSION_SUFFIX}.dll HINTS ${QUAZIP_LIBRARY_DIR})
-		FIND_PATH(QUAZIP_INCLUDE_DIR NAMES quazip.h HINTS ${QUAZIP_LIBRARY_DIR}/../ PATH_SUFFIXES include/quazip)
-		FIND_PATH(QUAZIP_ZLIB_INCLUDE_DIR NAMES zlib.h)
-	ELSE(WIN32)
-		FIND_PACKAGE(PkgConfig)
-#     pkg_check_modules(PC_QCA2 QUIET qca2)
-		pkg_check_modules(PC_QUAZIP quazip)
-		FIND_LIBRARY(QUAZIP_LIBRARIES
-			WIN32_DEBUG_POSTFIX d
-            NAMES quazip${QUAZIP_LIB_VERSION_SUFFIX}
-			HINTS /usr/lib /usr/lib64
-		)
-		FIND_PATH(QUAZIP_INCLUDE_DIR quazip.h
-			HINTS /usr/include /usr/local/include
-			PATH_SUFFIXES quazip${QUAZIP_LIB_VERSION_SUFFIX}
-		)
-		FIND_PATH(QUAZIP_ZLIB_INCLUDE_DIR zlib.h HINTS /usr/include /usr/local/include)
-	ENDIF (WIN32)
-	INCLUDE(FindPackageHandleStandardArgs)
-	SET(QUAZIP_INCLUDE_DIRS ${QUAZIP_INCLUDE_DIR} ${QUAZIP_ZLIB_INCLUDE_DIR})
-	find_package_handle_standard_args(QUAZIP DEFAULT_MSG  QUAZIP_LIBRARIES QUAZIP_INCLUDE_DIR QUAZIP_ZLIB_INCLUDE_DIR QUAZIP_INCLUDE_DIRS)
-ENDIF (QUAZIP_INCLUDE_DIRS AND QUAZIP_LIBRARIES)
+find_path(
+    QuaZip_INCLUDE_DIR quazip5/quazip.h
+    PATHS $ENV{QUAZIP_DIR}
+          $ENV{QUAZIP_ROOT}
+    PATH_SUFFIXES include
+)
+
+find_library(
+    QuaZip_LIBRARY_RELEASE quazip
+    PATHS $ENV{QUAZIP_DIR}
+          $ENV{QUAZIP_ROOT}
+    PATH_SUFFIXES lib
+)
+
+find_library(
+    QuaZip_LIBRARY_DEBUG quazipd
+    PATHS $ENV{QUAZIP_DIR}
+          $ENV{QUAZIP_ROOT}
+    PATH_SUFFIXES lib
+)
+
+if(QuaZip_LIBRARY_RELEASE OR QuaZip_LIBRARY_DEBUG)
+    set(
+        QuaZip_LIBRARIES
+        release ${QuaZip_LIBRARY_RELEASE}
+	debug ${QuaZip_LIBRARY_DEBUG}
+    )
+endif()
+
+if(QuaZip_INCLUDE_DIR)
+    set(
+        QuaZip_INCLUDE_DIRS
+        ${QuaZip_INCLUDE_DIR}
+    )
+endif()
+
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    QuaZip FOUND_VAR QuaZip_FOUND
+    REQUIRED_VARS QuaZip_INCLUDE_DIRS QuaZip_LIBRARIES
+    VERSION_VAR QuaZip_VERSION_STRING
+)
