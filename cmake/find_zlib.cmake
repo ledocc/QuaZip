3118501f1eb6ee529d6d398d8182af08390b8cfa
David Callu <callu.david@gmail.com>
2016-07-01 14:00:38 +0200

    chore(dependencies): search original zlib before search the one in Qt

--- cmake/find_zlib.cmake
+++ cmake/find_zlib.cmake
@@ -1,19 +1,16 @@
 
 
-# Use system zlib on unix and Qt ZLIB on Windows
-if(MSVC)
+find_package(ZLIB REQUIRED)
 
-    # is Qt build with embeded zlib
+
+if(MSVC AND NOT ZLIB_INCLUDE_DIRS)
+    
     find_path(
         ZLIB_INCLUDE_DIRS zlib.h
         HINTS ${Qt5Core_INCLUDE_DIRS}
         PATH_SUFFIXES QtZlib
         NO_DEFAULT_PATH
         )
+    
 endif()
 
-if((UNIX OR MINGW) OR (NOT ZLIB_INCLUDE_DIRS))
-
-    find_package(ZLIB REQUIRED)
-
-endif()
