c64ed6d238d9e5373404c2fc6a673d7b502f7f36
David Callu <callu.david@gmail.com>
2017-02-02 15:50:50 +0100

    feat(cmake): use ZLIB::ZLIB dependencies to have quazip installation relocatable

--- cmake/find_zlib.cmake
+++ cmake/find_zlib.cmake
@@ -1,16 +1,3 @@
 
 
 find_package(ZLIB REQUIRED)
-
-
-if(MSVC AND NOT ZLIB_INCLUDE_DIRS)
-    
-    find_path(
-        ZLIB_INCLUDE_DIRS zlib.h
-        HINTS ${Qt5Core_INCLUDE_DIRS}
-        PATH_SUFFIXES QtZlib
-        NO_DEFAULT_PATH
-        )
-    
-endif()
-
